a4b922feeb9aca9a3b500c0028f708b29d40d98f
pookiedog11 <149290576+psychorax1433@users.noreply.github.com>
2025-02-08 16:54:30 +0530

    Add files via upload

---
+++ mom.py
@@ -0,0 +1,8 @@
+maths = 11
+socialstudy= 1000
+hindi= 111
+sum = maths+socialstudy+hindi
+print("the sum of all the  subjests is: ", sum)
+
+average = sum/3
+print("the average of all the subjects  is :", average)
